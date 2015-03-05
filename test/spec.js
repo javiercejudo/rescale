8dd4efccb09d297873b78446b328b7a68d5fae2d
Javier Cejudo <javier@javiercejudo.com>
2015-03-05 23:32:15 +1100

    test(rescale): make composition test more robust

--- test/spec.js
+++ test/spec.js
@@ -34,12 +34,18 @@ describe('rescaling', function() {
   });
 
   describe('with scales', function() {
-    var scaleMock;
+    var scaleMock, normaliseMock;
 
     beforeEach(function() {
-      sinon.stub(normalise, 'normalise').returns(21);
+      normaliseMock = sinon.mock(normalise);
+      normaliseMock.expects('normalise')
+        .withExactArgs('anything', [0, 100])
+        .returns(21);
+
       scaleMock = sinon.mock(scale);
-      scaleMock.expects('scale').withExactArgs(21, [32, 212]);
+      scaleMock.expects('scale')
+        .withExactArgs(21, [32, 212])
+        .returns(5);
     });
 
     afterEach(function() {
@@ -49,7 +55,7 @@ describe('rescaling', function() {
     });
 
     it('should compose normalise and scale', function() {
-      rescale('anything', 'random', [32, 212]);
+      rescale('anything', [0, 100], [32, 212]).should.be.exactly(5);
     });
   });
 });
